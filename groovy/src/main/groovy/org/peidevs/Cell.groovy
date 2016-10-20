a4e9f254fa2d5ff2303f1f1a9cd63930b2b0ae76
Michael Easter <codetojoy@gmail.com>
2016-10-20 10:25:25 -0300

    new Groovy project

---
+++ groovy/src/main/groovy/org/peidevs/Cell.groovy
@@ -0,0 +1,9 @@
+
+// http://peidevs.github.io
+
+package org.peidevs
+
+class Cell {
+    // bogus field to illustrate test
+    def foo
+}
---
+++ groovy/src/test/groovy/org/peidevs/CellTestCase.groovy
@@ -0,0 +1,19 @@
+
+// http://peidevs.github.io
+
+package org.peidevs
+
+import org.junit.*
+import static org.junit.Assert.*
+
+class CellTestCase {
+    @Test
+    public void testCanary() {
+        def cell = new Cell()
+
+        // test
+        cell.setFoo(5150)
+
+        assertEquals(5150, cell.foo)
+    }
+}
