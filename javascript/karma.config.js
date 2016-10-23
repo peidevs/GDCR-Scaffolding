b48d1095ff3f2471f0c9f13a1dbe72e9222f529e
Michael Easter <codetojoy@gmail.com>
2016-10-23 18:44:56 -0300

    incremental change as part of a larger frenzy of activity

--- javascript/karma.config.js
+++ javascript/karma.config.js
@@ -42,15 +42,6 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-/*
-    customLaunchers: {
-        Chrome_travis_ci: {
-          base: 'Chrome',
-          flags: ['--no-sandbox']
-        }
-    },
-*/
-
     // Start these browsers, currently available:
     // - Chrome
     // - ChromeCanary
@@ -64,15 +55,10 @@ module.exports = function(config) {
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
 
-
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false
   };
-
-  if (process.env.TRAVIS) {
-    cfg.browsers = ['Chrome_travis_ci']
-  }
 
   config.set(cfg);
 };
